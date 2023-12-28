2bb25baf5fad36095de89daf9984e3fad278cfe9
haotien91 <tim911004@gmail.com>
2023-12-28 13:47:20 +0800

    global update

--- global.cuh
+++ global.cuh
@@ -7,14 +7,14 @@
 #include <vector_types.h>
 #include <omp.h>
 
-
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
+#define POSITION(x, y, k) ((y) * (k) + (x))
 
 typedef struct _pos
 {
-    int x; 
+    int x;
     int y;
-}pos;
+} pos;
--- street.cuh
+++ street.cuh
@@ -10,23 +10,15 @@ public:
     {
         printf("Street Initialized\n");
         streetmap = new int *[MAP_SIZE];
-        tmp_streetmap = new int *[MAP_SIZE];
 
         for (int i = 0; i < MAP_SIZE; i++)
         {
             streetmap[i] = new int[MAP_SIZE]; // create an 64x64 map
-            tmp_streetmap[i] = new int[MAP_SIZE];
         }
     }
-    void Update_map();
     void Load_map(int *infile);
 
-private:
     int **streetmap;
-    int **tmp_streetmap;
-};
 
-void street::Update_map()
-{
-    // swap
-}
+private:
+};

83ff5be85bf53180ac403e96337cb3c42c37d2d8
haotien91 <tim911004@gmail.com>
2023-12-28 13:09:49 +0800

    street.cu update

--- street.cu
+++ street.cu
@@ -1,2 +1,21 @@
-#include"global.cuh"
-#include"street.cuh"
+#include "global.cuh"
+#include "street.cuh"
+
+void street::Read_map(int *infile)
+{
+    FILE *file = fopen(infile, "rb");
+
+    int tmp[16][16];
+
+    for (int i = 0; i < 16; i++)
+    {
+        fread(&tmp[i], sizeof(int), 16, file);
+    }
+
+    for (int i = 0; i < MAP_SIZE; i++)
+    {
+        for (int j = 0; j < MAP_SIZE; j++)
+        {
+        }
+    }
+}

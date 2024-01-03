7a50959d7e997e55cccf4e17ae286850def98ea9
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-03 21:53:46 +0800

    Update street.cu

--- street.cu
+++ street.cu
@@ -5,8 +5,8 @@ void street::Load_map(char *infilename)
     FILE *file = fopen(infilename, "rb");
     int inp_size = MAP_SIZE / SCALE_SIZE;
 
-    this->streetmap = (int *)malloc(inp_size * inp_size);
-    this->Outputmap = (int *)malloc(MAP_SIZE * MAP_SIZE);
+    this->streetmap = (int *)malloc(inp_size * inp_size *sizeof(int));
+    this->Outputmap = (int *)malloc(MAP_SIZE * MAP_SIZE *sizeof(int));
 
     fread(this->streetmap, sizeof(int), inp_size * inp_size, file);
 
@@ -25,7 +25,7 @@ void street::Load_map(char *infilename)
     cudaMemcpy(this->Dstreetmap, this->streetmap, inp_size * inp_size * sizeof(int), cudaMemcpyHostToDevice);
 
     cudaMalloc((void **)&this->Dscaled_map, MAP_SIZE * MAP_SIZE * sizeof(map));
-    cudaMalloc((void **)&this->DOutputmap, MAP_SIZE * MAP_SIZE * sizeof(this->Outputmap));
+    cudaMalloc((void **)&this->DOutputmap, MAP_SIZE * MAP_SIZE * sizeof(int));
 
     printf("loaded input  map , inp size %d \n", inp_size);
 }
@@ -33,14 +33,30 @@ void street::Load_map(char *infilename)
 void street::Output_map(char *outfilename)
 {
 
-    cudaMemcpy(this->Outputmap, this->DOutputmap, MAP_SIZE * MAP_SIZE * sizeof(this->Outputmap), cudaMemcpyDeviceToHost);
+    cudaMemcpy(this->Outputmap, this->DOutputmap, MAP_SIZE * MAP_SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+
+    // char newline = '\n';
 
     // if a phase is ended. output current map.
     FILE *outfile = fopen(outfilename, "ab");
 
     fwrite(this->Outputmap, sizeof(int), MAP_SIZE * MAP_SIZE, outfile);
+    // fwrite(&newline, sizeof(char), 1, outfile);
 
     fclose(outfile);
 
     return;
+}
+
+void street::Output_size(char *outfilename)
+{
+    FILE *outfile = fopen(outfilename, "w");
+
+    // char blank = ' ';
+    // char newline = '\n';
+
+    fwrite(&this->size, sizeof(int), 1, outfile);
+    // fwrite(&blank, sizeof(char), 1, outfile);
+    // fwrite(&this->height, sizeof(int), 1, outfile);
+    // fwrite(&newline, sizeof(char), 1, outfile);
 }

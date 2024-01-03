74c299cd8da695335d1f15def2fd16cca5e7e32e
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-03 23:14:48 +0800

    Update person.cu

--- person.cu
+++ person.cu
@@ -4,38 +4,55 @@ __device__ person::person(int direction, pos position, int speed, preference p)
 {
     this->direction = direction;
     this->position = position;
+    this->next_position = position;
     this->speed = speed;
     this->p = p;
 
     return;
 }
 
-__device__ void
+__device__ int
 person::decide(map *Dscaled_map)
 {
     int choice = this->p.choose();
     switch (choice)
     {
     case UP:
+        //  this->next_position.x = this->position.x;
         this->next_position.y = this->position.y - this->speed;
         break;
     case DOWN:
+        //   this->next_position.x = this->position.x;
         this->next_position.y = this->position.y + this->speed;
         break;
     case LEFT:
         this->next_position.x = this->position.x - this->speed;
+        //     this->next_position.y = this->position.y;
         break;
     case RIGHT:
         this->next_position.x = this->position.x + this->speed;
+        //     this->next_position.y = this->position.y;
         break;
     }
 
-    if (!is_walkable(Dscaled_map, this->position))
-        this->next_position = this->position;
+    if (!is_walkable(Dscaled_map, this->next_position))
+    {
+        this->next_position.x = this->position.x;
+        this->next_position.y = this->position.y;
+    }
     else
+    {
+        // below line should not happen :
+        // if(choice == UP)printf("%d %d %d %d %d %d\n", this->next_position.x,this->position.x, this->next_position.y,this->position.y,C(this->next_position.x,this->next_position.y,MAP_SIZE),Dscaled_map[C(this->next_position.x,this->next_position.y,MAP_SIZE)].vis);
         this->direction = choice;
 
-    return;
+        // whether person is out of bound?
+        if (this->next_position.x < 0 || this->next_position.x >= MAP_SIZE || this->next_position.y < 0 || this->next_position.y >= MAP_SIZE)
+        {
+            return REMOVE;
+        }
+    }
+    return ON_BOARD;
 }
 
 __device__ void
@@ -55,9 +72,11 @@ person::walk_back(map *Dscaled_map)
 }
 
 __device__ bool
-person::is_walkable(map *Dstreetmap, pos position_check)
+person::is_walkable(map *Dscaled_map, pos position_check)
 {
-    if (Dstreetmap[C(position_check.x, position_check.y, MAP_SIZE)].vis == -1)
+    if (C(position_check.x, position_check.y, MAP_SIZE) < 0 || C(position_check.x, position_check.y, MAP_SIZE) >= MAP_SIZE * MAP_SIZE)
+        return false;
+    if (Dscaled_map[C(position_check.x, position_check.y, MAP_SIZE)].vis == -1)
         return true;
     else
         return false;

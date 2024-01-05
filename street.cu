d116ba34fad15a18860de5f8c083c515a3e43326
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-05 23:33:39 +0800

    old

--- street.cu
+++ street.cu
@@ -1,9 +1,5 @@
 #include "street.cuh"
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
-
 void street::Load_map(char *infilename)
 {
     FILE *file = fopen(infilename, "rb");
@@ -34,98 +30,6 @@ void street::Load_map(char *infilename)
     printf("loaded input  map , inp size %d \n", inp_size);
 }
 
-void street::Set_bounds()
-{
-    int x_count = 0 ,y_count = 0,state  ;
-    int inp_size = MAP_SIZE / SCALE_SIZE;
-
-    this->x_bounds = (int*)malloc(inp_size * inp_size * sizeof(int));
-    this->y_bounds = (int*)malloc(inp_size * inp_size * sizeof(int));
-    memset(this->x_bounds,-1, inp_size * inp_size * sizeof(int));
-    memset(this->y_bounds,-1, inp_size * inp_size * sizeof(int));
-    
-    for(int i = 0 ; i < inp_size ; i++ )
-    {
-        state = this->streetmap[C(0,i,inp_size)];
-        x_count = 0;
-        for(int j = 0 ; j < inp_size ;j++)
-        {
-            if(this->streetmap[C(j,i,inp_size)] != state)
-            {
-                if(state == 0)
-                {
-                   if(this->x_bounds[C(x_count,i,inp_size)] != j-1)this->x_bounds[C(x_count++,i,inp_size)]  = j-1;
-                   state  = 1;
-                }
-                else
-                {
-                    this->x_bounds[C(x_count++,i,inp_size)] = j;
-                    state  = 0;
-                }
-            }
-        }
-        qsort(this->x_bounds,x_count,sizeof(int),cmpfunc);
-    }
-
-    for(int i = 0 ; i < inp_size ; i++ )
-    {
-        state = this->streetmap[C(i,0,inp_size)];
-        y_count = 0;
-        for(int j = 0 ; j < inp_size ;j++)
-        {
-            if(this->streetmap[C(i,j,inp_size)] != state)
-            {
-                if(state == 0)
-                {
-                   if(this->y_bounds[C(y_count,i,inp_size)] != j-1)this->y_bounds[C(y_count++,i,inp_size)] = j-1;
-                   state  = 1;
-                }
-                else
-                {
-                    this->y_bounds[C(y_count++,i,inp_size)] = j;
-                    state  = 0;
-                }
-            }
-        }
-        qsort(this->y_bounds,y_count,sizeof(int),cmpfunc);
-    }
-
-    printf("y_bounds:\n");
-    for(int i= 0 ; i< inp_size ;i++)
-    {
-        for(int j =0 ; j< inp_size ;j++)
-        {
-            printf("%d ", this->y_bounds[C(j,i,inp_size)]);
-        }
-        printf("\n");
-    }
-
-    printf("x_bounds:\n"); 
-    for(int i= 0 ; i< inp_size ;i++)
-    {
-        for(int j =0 ; j< inp_size ;j++)
-        {
-            printf("%d ", this->x_bounds[C(j,i,inp_size)]);
-        }
-        printf("\n");
-    }
-
-    //SCALING 
-    for(int i = 0 ; i < inp_size * inp_size ; i++ )
-    {
-        this -> x_bounds[i] = this -> x_bounds[i] * SCALE_SIZE + (SCALE_SIZE-1);
-        this -> y_bounds[i] = this -> y_bounds[i] * SCALE_SIZE + (SCALE_SIZE-1);
-    }
-    
-    cudaMalloc((void **)&this->Dx_bounds, inp_size * inp_size * sizeof(int));
-    cudaMemcpy(this->Dx_bounds, this->x_bounds, inp_size * inp_size * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMalloc((void **)&this->Dy_bounds, inp_size * inp_size * sizeof(int));
-    cudaMemcpy(this->Dy_bounds, this->y_bounds, inp_size * inp_size * sizeof(int), cudaMemcpyHostToDevice);
-
-    
-
-
-}
 void street::Output_map(char *outfilename)
 {
 
